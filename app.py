393d5ab5b424dd4bc7fb4f85d9bea589cfde1ff6
andrewsleadco <andrew@andrewjaquez.com>
2025-04-23 04:41:09 +0000

    Adds files for front end

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import eng_to_ipa as ipa
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        text = request.form['text']
+        result = ipa.convert(text, stress_marks='both')
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- test_script.py
+++ test_script.py
@@ -1,6 +1,6 @@
 import eng_to_ipa as ipa
 
-sentence = "This is a fuckign test sentence and it lives inside this script."
+sentence = "Well, here's a story for you: Sarah Perry was a veterinary nurse who had been working daily at an old zoo in a deserted district of the territory, so she was very happy to start a new job at a superb private practice in North Square near the Duke Street Tower. That area was much nearer for her and more to her liking. Even so, on her first morning, she felt stressed. She ate a bowl of porridge, checked herself in the mirror and washed her face in a hurry. Then she put on a plain yellow dress and a fleece jacket, picked up her kit and headed for work. "
 result = ipa.convert(sentence, retrieve_all=False, stress_marks=False)
 
 print(result)
