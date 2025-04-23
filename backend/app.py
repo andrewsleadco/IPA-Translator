ea67df3b195bd4785f155b8665ea58fc6d463cd7
andrewsleadco <andrew@andrewjaquez.com>
2025-04-23 05:25:19 +0000

    add debugging to flask and updates html

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask, render_template, request
 import eng_to_ipa as ipa
+from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder=os.path.join(os.path.dirname(__file__), '../frontend'),
+    static_folder=os.path.join(os.path.dirname(__file__), '../frontend/static')
+)
+
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -10,3 +18,7 @@ def index():
         text = request.form['text']
         result = ipa.convert(text, stress_marks='both')
     return render_template('index.html', result=result)
+
+if __name__ == "__main__":
+    print("Starting Flask app on http://localhost:5000")
+    app.run(debug=True)
